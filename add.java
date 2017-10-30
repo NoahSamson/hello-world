e122da625395bae7d856b3a1093e01cd0821e161
Your Name <you@example.com>
2017-10-30 20:00:06 +0530

    addition

---
+++ add.java
@@ -0,0 +1,11 @@
+public class add {
+    public static void main(String[] args){
+        int num1,num2,num3;
+        num1 =10;
+        num2 = 48;
+
+        num3 = num1 + num2 ;
+
+        System.out.println("Addition of "+num1+" and "+num2+"is:-"+num3);
+    }
+}
