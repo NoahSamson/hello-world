be8b1e4ddd3f4dd2cafa2017b942be4382bced51
Ya-dola <Ya-dola@users.noreply.github.com>
2018-10-14 21:40:02 +0300

    Adding Age to Person Object

--- java/Person.java
+++ java/Person.java
@@ -1,16 +1,26 @@
 class Person {
 	String name;
+	int age;
 
-	public Person(String name) {
+	public Person(String name, int age) {
 		super();
 		this.name = name;
+		this.age = age;
 	}
 
 	public static void main(String[] args) {
-		Person person = new Person("Aadil");
+		Person person = new Person("Aadil", 20);
 
-		System.out.println(person.getName());
+		System.out.println("Name: " + person.getName() + "\n Age: " + person.getAge());
 
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
 	}
 
 	public String getName() {
