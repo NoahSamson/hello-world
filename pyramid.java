4a6e2e283c22f4a073764da1da9e203861a4e9dc
thivatm <thivagartm@gmail.com>
2018-10-30 17:15:17 +0530

    Pyramid function

---
+++ pyramid.java
@@ -0,0 +1,20 @@
+public class pyramid{
+    public static void main(String[] args){
+       int n = 5; 
+       for (int i=0; i<n; i++) 
+        { 
+            for (int j=n-i; j>1; j--) 
+            { 
+                // printing spaces 
+                System.out.print(" "); 
+            }  
+            for (int j=0; j<=i; j++ ) 
+            { 
+                // printing stars 
+                System.out.print("* "); 
+            } 
+            System.out.println(); 
+        } 
+    }
+
+}
